f48f57abaad096d8b100d1f2b8ccb5186eee107f
Joe <joecgo@gmail.com>
2021-03-15 00:14:56 -0700

    move stb location

--- Source/Graphics/Model.hpp
+++ Source/Graphics/Model.hpp
@@ -2,12 +2,12 @@
 #define MODEL_H
 
 #define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
 
 #include <glad/glad.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <assimp/stb_image.h>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
