805fe2d9eb38240bf21495c8ac709884c2579a05
Joe <joecgo@gmail.com>
2021-03-15 18:38:06 -0700

    splitting up into cpp and hpp files

--- Source/Core/main.cpp
+++ Source/Core/main.cpp
@@ -11,239 +11,242 @@
 #include <Graphics/Mesh.hpp>
 #include <Graphics/Model.hpp>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void processInput(GLFWwindow* window);
-
-// settings
-const unsigned int SCR_WIDTH = 1200;
-const unsigned int SCR_HEIGHT = 900;
-
-// mouse input
-bool firstMouse = true;
-float lastX = SCR_WIDTH / 2.0;
-float lastY = SCR_HEIGHT / 2.0;
-
-//camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-
-// timing
-float deltaTime = 0.0f;	// time between current frame and last frame
-float lastFrame = 0.0f;
-
-struct Color
+namespace GenevaEngine
 {
-	float r = 0.0f, g = 0.0f, b = 0.0f;
-	Color(int hexValue)
+	void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+	void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+	void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+	void processInput(GLFWwindow* window);
+
+	// settings
+	const unsigned int SCR_WIDTH = 1200;
+	const unsigned int SCR_HEIGHT = 900;
+
+	// mouse input
+	bool firstMouse = true;
+	float lastX = SCR_WIDTH / 2.0;
+	float lastY = SCR_HEIGHT / 2.0;
+
+	//camera
+	Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+	// timing
+	float deltaTime = 0.0f;	// time between current frame and last frame
+	float lastFrame = 0.0f;
+
+	struct Color
 	{
-		r = ((hexValue >> 16) & 0xFF) / 255.0f;
-		g = ((hexValue >> 8) & 0xFF) / 255.0f;
-		b = ((hexValue) & 0xFF) / 255.0f;
+		float r = 0.0f, g = 0.0f, b = 0.0f;
+		Color(int hexValue)
+		{
+			r = ((hexValue >> 16) & 0xFF) / 255.0f;
+			g = ((hexValue >> 8) & 0xFF) / 255.0f;
+			b = ((hexValue) & 0xFF) / 255.0f;
+		}
+	};
+
+	Color palette[]{
+	  Color(0x7eed94),
+	  Color(0x1a2c79),
+	  Color(0xe6a26e),
+	  Color(0x64a6ea),
+	  Color(0xa163a2),
+	};
+
+	glm::vec3 generateRandRotAxis()
+	{
+		return glm::vec3(
+			(rand() % 200) - 100 / 100.0f,
+			(rand() % 200) - 100 / 100.0f,
+			(rand() % 200) - 100 / 100.0f
+		);
 	}
-};
 
-Color palette[]{
-  Color(0x7eed94),
-  Color(0x1a2c79),
-  Color(0xe6a26e),
-  Color(0x64a6ea),
-  Color(0xa163a2),
-};
-
-glm::vec3 generateRandRotAxis()
-{
-	return glm::vec3(
-		(rand() % 200) - 100 / 100.0f,
-		(rand() % 200) - 100 / 100.0f,
-		(rand() % 200) - 100 / 100.0f
-	);
-}
-
-void setClearColor(Color color)
-{
-	glClearColor(color.r, color.g, color.b, 1.0f);
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow* window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-
-	if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
-		setClearColor(Color(000000));
-	if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
-		setClearColor(palette[0]);
-	if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
-		setClearColor(palette[1]);
-	if (glfwGetKey(window, GLFW_KEY_4) == GLFW_PRESS)
-		setClearColor(palette[2]);
-	if (glfwGetKey(window, GLFW_KEY_5) == GLFW_PRESS)
-		setClearColor(palette[3]);
-	if (glfwGetKey(window, GLFW_KEY_6) == GLFW_PRESS)
-		setClearColor(palette[4]);
-
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		camera.ProcessKeyboard(Camera_Movement::FORWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.ProcessKeyboard(Camera_Movement::BACKWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.ProcessKeyboard(Camera_Movement::LEFT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.ProcessKeyboard(Camera_Movement::RIGHT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-		camera.ProcessKeyboard(Camera_Movement::DOWN, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
-		camera.ProcessKeyboard(Camera_Movement::UP, deltaTime);
-}
-
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-	if (firstMouse)
+	void setClearColor(Color color)
 	{
+		glClearColor(color.r, color.g, color.b, 1.0f);
+	}
+
+	void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+	{
+		glViewport(0, 0, width, height);
+	}
+
+	void processInput(GLFWwindow* window)
+	{
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, true);
+
+		if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+		if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+			setClearColor(Color(000000));
+		if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+			setClearColor(palette[0]);
+		if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
+			setClearColor(palette[1]);
+		if (glfwGetKey(window, GLFW_KEY_4) == GLFW_PRESS)
+			setClearColor(palette[2]);
+		if (glfwGetKey(window, GLFW_KEY_5) == GLFW_PRESS)
+			setClearColor(palette[3]);
+		if (glfwGetKey(window, GLFW_KEY_6) == GLFW_PRESS)
+			setClearColor(palette[4]);
+
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+			camera.ProcessKeyboard(Camera::Movement::FORWARD, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+			camera.ProcessKeyboard(Camera::Movement::BACKWARD, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+			camera.ProcessKeyboard(Camera::Movement::LEFT, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+			camera.ProcessKeyboard(Camera::Movement::RIGHT, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+			camera.ProcessKeyboard(Camera::Movement::DOWN, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+			camera.ProcessKeyboard(Camera::Movement::UP, deltaTime);
+	}
+
+	void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+	{
+		if (firstMouse)
+		{
+			lastX = xpos;
+			lastY = ypos;
+			firstMouse = false;
+		}
+
+		float xoffset = xpos - lastX;
+		float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
 		lastX = xpos;
 		lastY = ypos;
-		firstMouse = false;
+
+		float sensitivity = 0.1f; // change this value to your liking
+		xoffset *= sensitivity;
+		yoffset *= sensitivity;
+
+		camera.ProcessMouseMovement(xoffset, yoffset);
 	}
 
-	float xoffset = xpos - lastX;
-	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-	lastX = xpos;
-	lastY = ypos;
-
-	float sensitivity = 0.1f; // change this value to your liking
-	xoffset *= sensitivity;
-	yoffset *= sensitivity;
-
-	camera.ProcessMouseMovement(xoffset, yoffset);
-}
-
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-	camera.ProcessMouseScroll(yoffset);
-}
-
-int main()
-{
-	// glfw: initialize and configure
-	// ------------------------------
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	// glfw window creation
-	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "GenevaEngine", NULL, NULL);
-	if (window == NULL)
+	void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	glfwSetCursorPosCallback(window, mouse_callback);
-	glfwSetScrollCallback(window, scroll_callback);
-
-	// tell GLFW to capture our mouse
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-	// glad: load all OpenGL function pointers
-	// ---------------------------------------
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
+		camera.ProcessMouseScroll(yoffset);
 	}
 
-	// build and compile shaders
-	// -------------------------
-	Shader textureShader("Shaders/Shader.vert", "Shaders/Shader.frag");
-	Shader greyShader("Shaders/Shader.vert", "Shaders/GreyShader.frag");
-
-	// tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
-	stbi_set_flip_vertically_on_load(true);
-
-	// configure global opengl state
-	// -----------------------------
-	glEnable(GL_DEPTH_TEST);
-
-	// load model
-// -----------
-	Model testModel("Assets/Art/Test/Backpack/backpack.obj");
-	Model kevinModel("Assets/Art/Test/Kevin/Kevin.obj");
-
-	// render loop
-	// -----------
-	while (!glfwWindowShouldClose(window))
+	int main()
 	{
-		// per-frame time logic
+		// glfw: initialize and configure
+		// ------------------------------
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+		// glfw window creation
 		// --------------------
-		float currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
+		GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "GenevaEngine", NULL, NULL);
+		if (window == NULL)
+		{
+			std::cout << "Failed to create GLFW window" << std::endl;
+			glfwTerminate();
+			return -1;
+		}
+		glfwMakeContextCurrent(window);
+		glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+		glfwSetCursorPosCallback(window, mouse_callback);
+		glfwSetScrollCallback(window, scroll_callback);
 
-		// input
-		// -----
-		processInput(window);
+		// tell GLFW to capture our mouse
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-		// render
-		// ------
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		// glad: load all OpenGL function pointers
+		// ---------------------------------------
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+		{
+			std::cout << "Failed to initialize GLAD" << std::endl;
+			return -1;
+		}
 
-		// practice with coordinate systems and camera view
-		textureShader.use();
-		glm::mat4 view = camera.GetViewMatrix();
-		glm::mat4 projection;
-		projection = glm::perspective(glm::radians(camera.Fov),
-			(float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		int viewLoc = glGetUniformLocation(textureShader.ID, "view");
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		int projectionLoc = glGetUniformLocation(textureShader.ID, "projection");
-		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+		// build and compile shaders
+		// -------------------------
+		Shader textureShader("Shaders/Shader.vert", "Shaders/Shader.frag");
+		Shader greyShader("Shaders/Shader.vert", "Shaders/GreyShader.frag");
 
-		// render the test model
-		glm::mat4 model1 = glm::mat4(1.0f);
-		model1 = glm::translate(model1, glm::vec3(0.0f, 0.0f, 0.0f));
-		model1 = glm::scale(model1, glm::vec3(1.0f, 1.0f, 1.0f));
-		int modelLoc1 = glGetUniformLocation(textureShader.ID, "model");
-		glUniformMatrix4fv(modelLoc1, 1, GL_FALSE, glm::value_ptr(model1));
-		testModel.Draw(textureShader);
+		// tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+		stbi_set_flip_vertically_on_load(true);
 
-		// practice with coordinate systems and camera view
-		greyShader.use();
-		int viewLoc2 = glGetUniformLocation(greyShader.ID, "view");
-		glUniformMatrix4fv(viewLoc2, 1, GL_FALSE, glm::value_ptr(view));
-		int projectionLoc2 = glGetUniformLocation(greyShader.ID, "projection");
-		glUniformMatrix4fv(projectionLoc2, 1, GL_FALSE, glm::value_ptr(projection));
+		// configure global opengl state
+		// -----------------------------
+		glEnable(GL_DEPTH_TEST);
 
-		// render kevin
-		glm::mat4 model2 = glm::mat4(1.0f);
-		model2 = glm::translate(model2, glm::vec3(5.0f, -2.0f, 0.0f));
-		model2 = glm::scale(model2, glm::vec3(5.0f, 5.0f, 5.0f));
-		int modelLoc2 = glGetUniformLocation(greyShader.ID, "model");
-		glUniformMatrix4fv(modelLoc2, 1, GL_FALSE, glm::value_ptr(model2));
-		kevinModel.Draw(greyShader);
+		// load model
+	// -----------
+		Model testModel("Assets/Art/Test/Backpack/backpack.obj");
+		Model kevinModel("Assets/Art/Test/Kevin/Kevin.obj");
 
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+		// render loop
+		// -----------
+		while (!glfwWindowShouldClose(window))
+		{
+			// per-frame time logic
+			// --------------------
+			float currentFrame = glfwGetTime();
+			deltaTime = currentFrame - lastFrame;
+			lastFrame = currentFrame;
+
+			// input
+			// -----
+			processInput(window);
+
+			// render
+			// ------
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			// practice with coordinate systems and camera view
+			textureShader.use();
+			glm::mat4 view = camera.GetViewMatrix();
+			glm::mat4 projection;
+			projection = glm::perspective(glm::radians(camera.Fov),
+				(float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+			int viewLoc = glGetUniformLocation(textureShader.ID, "view");
+			glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+			int projectionLoc = glGetUniformLocation(textureShader.ID, "projection");
+			glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+			// render the test model
+			glm::mat4 model1 = glm::mat4(1.0f);
+			model1 = glm::translate(model1, glm::vec3(0.0f, 0.0f, 0.0f));
+			model1 = glm::scale(model1, glm::vec3(1.0f, 1.0f, 1.0f));
+			int modelLoc1 = glGetUniformLocation(textureShader.ID, "model");
+			glUniformMatrix4fv(modelLoc1, 1, GL_FALSE, glm::value_ptr(model1));
+			testModel.Draw(textureShader);
+
+			// practice with coordinate systems and camera view
+			greyShader.use();
+			int viewLoc2 = glGetUniformLocation(greyShader.ID, "view");
+			glUniformMatrix4fv(viewLoc2, 1, GL_FALSE, glm::value_ptr(view));
+			int projectionLoc2 = glGetUniformLocation(greyShader.ID, "projection");
+			glUniformMatrix4fv(projectionLoc2, 1, GL_FALSE, glm::value_ptr(projection));
+
+			// render kevin
+			glm::mat4 model2 = glm::mat4(1.0f);
+			model2 = glm::translate(model2, glm::vec3(5.0f, -2.0f, 0.0f));
+			model2 = glm::scale(model2, glm::vec3(5.0f, 5.0f, 5.0f));
+			int modelLoc2 = glGetUniformLocation(greyShader.ID, "model");
+			glUniformMatrix4fv(modelLoc2, 1, GL_FALSE, glm::value_ptr(model2));
+			kevinModel.Draw(greyShader);
+
+			// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+			// -------------------------------------------------------------------------------
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
+
+		// glfw: terminate, clearing all previously allocated GLFW resources.
+		// ------------------------------------------------------------------
+		glfwTerminate();
+		return 0;
 	}
-
-	// glfw: terminate, clearing all previously allocated GLFW resources.
-	// ------------------------------------------------------------------
-	glfwTerminate();
-	return 0;
 }
--- Source/Graphics/Camera.hpp
+++ Source/Graphics/Camera.hpp
@@ -7,144 +7,76 @@
 
 #include <vector>
 
-// Defines several possible options for camera movement.
-enum class Camera_Movement
+namespace GenevaEngine
 {
-	FORWARD,
-	BACKWARD,
-	LEFT,
-	RIGHT,
-	UP,
-	DOWN
-};
-
-// Default camera values
-const float YAW = -90.0f;
-const float PITCH = 0.0f;
-const float SPEED = 2.5f;
-const float SENSITIVITY = 0.3f;
-const float FOV = 70.0f;
-const float MAXFOV = 120.0f;
-
-// An abstract camera class that processes input and calculates the corresponding Euler Angles,
-// Vectors and Matrices for use in OpenGL
-class Camera
-{
-public:
-	// camera Attributes
-	glm::vec3 Position;
-	glm::vec3 Front;
-	glm::vec3 Up;
-	glm::vec3 Right;
-	glm::vec3 WorldUp;
-	// euler Angles
-	float Yaw;
-	float Pitch;
-	// camera options
-	float MovementSpeed;
-	float MouseSensitivity;
-	float Fov;
-
-	// constructor with vectors
-	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
-		glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
-		float yaw = YAW,
-		float pitch = PITCH) :
-		Front(glm::vec3(0.0f, 0.0f, -1.0f)),
-		MovementSpeed(SPEED),
-		MouseSensitivity(SENSITIVITY), Fov(FOV)
+	// An abstract camera class that processes input and calculates the corresponding Euler Angles,
+	// Vectors and Matrices for use in OpenGL
+	class Camera
 	{
-		Position = position;
-		WorldUp = up;
-		Yaw = yaw;
-		Pitch = pitch;
-		updateCameraVectors();
-	}
-	// constructor with scalar values
-	Camera(float posX, float posY, float posZ, float upX, float upY,
-		float upZ, float yaw, float pitch) :
-		Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED),
-		MouseSensitivity(SENSITIVITY), Fov(FOV)
-	{
-		Position = glm::vec3(posX, posY, posZ);
-		WorldUp = glm::vec3(upX, upY, upZ);
-		Yaw = yaw;
-		Pitch = pitch;
-		updateCameraVectors();
-	}
+	public:
 
-	// returns the view matrix calculated using Euler Angles and the LookAt Matrix
-	glm::mat4 GetViewMatrix()
-	{
-		return glm::lookAt(Position, Position + Front, Up);
-	}
-
-	// processes input received from any keyboard-like input system.
-	// Accepts input parameter in the form of camera defined ENUM
-	void ProcessKeyboard(Camera_Movement direction, float deltaTime)
-	{
-		float velocity = MovementSpeed * deltaTime;
-		if (direction == Camera_Movement::FORWARD)
-			Position += Front * velocity;
-		if (direction == Camera_Movement::BACKWARD)
-			Position -= Front * velocity;
-		if (direction == Camera_Movement::LEFT)
-			Position -= Right * velocity;
-		if (direction == Camera_Movement::RIGHT)
-			Position += Right * velocity;
-		if (direction == Camera_Movement::UP)
-			Position += Up * velocity;
-		if (direction == Camera_Movement::DOWN)
-			Position -= Up * velocity;
-	}
-
-	// processes input received from a mouse input system.
-	// Expects the offset value in both the x and y direction.
-	void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
-	{
-		xoffset *= MouseSensitivity;
-		yoffset *= MouseSensitivity;
-
-		Yaw += xoffset;
-		Pitch += yoffset;
-
-		// make sure that when pitch is out of bounds, screen doesn't get flipped
-		if (constrainPitch)
+		// Defines several possible options for camera movement.
+		enum class Movement
 		{
-			if (Pitch > 89.0f)
-				Pitch = 89.0f;
-			if (Pitch < -89.0f)
-				Pitch = -89.0f;
-		}
+			FORWARD,
+			BACKWARD,
+			LEFT,
+			RIGHT,
+			UP,
+			DOWN
+		};
 
-		// update Front, Right and Up Vectors using the updated Euler angles
-		updateCameraVectors();
-	}
+		// camera Attributes
+		glm::vec3 Position;
+		glm::vec3 Front;
+		glm::vec3 Up;
+		glm::vec3 Right;
+		glm::vec3 WorldUp;
 
-	// processes input received from a mouse scroll-wheel event.
-	// Only requires input on the vertical wheel-axis
-	void ProcessMouseScroll(float yoffset)
-	{
-		Fov -= (float)yoffset;
-		if (Fov < 1.0f)
-			Fov = 1.0f;
-		if (Fov > MAXFOV)
-			Fov = MAXFOV;
-	}
+		// Default camera values
+		static constexpr float YAW = -90.0f;
+		static constexpr float PITCH = 0.0f;
+		static constexpr float SPEED = 2.5f;
+		static constexpr float SENSITIVITY = 0.3f;
+		static constexpr float FOV = 70.0f;
 
-private:
-	// calculates the front vector from the Camera's (updated) Euler Angles
-	void updateCameraVectors()
-	{
-		// calculate the new Front vector
-		glm::vec3 front;
-		front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-		front.y = sin(glm::radians(Pitch));
-		front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-		Front = glm::normalize(front);
-		// also re-calculate the Right and Up vector
-		Right = glm::normalize(glm::cross(Front, WorldUp));
-		Up = glm::normalize(glm::cross(Right, Front));
-	}
-};
+		// euler Angles
+		float Yaw = -90.0f;
+		float Pitch = 0.0f;
+
+		// camera options
+		float MovementSpeed;
+		float MouseSensitivity;
+		float Fov;
+		float MaxFov = 150.0f;
+		float MinFov = 20.0f;
+
+		// constructor with vectors
+		Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
+			glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH);
+
+		// constructor with scalar values
+		Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw,
+			float pitch);
+
+		// returns the view matrix calculated using Euler Angles and the LookAt Matrix
+		glm::mat4 GetViewMatrix();
+
+		// processes input received from any keyboard-like input system.
+		// Accepts input parameter in the form of camera defined ENUM
+		void ProcessKeyboard(Movement direction, float deltaTime);
+
+		// processes input received from a mouse input system.
+		// Expects the offset value in both the x and y direction.
+		void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true);
+
+		// processes input received from a mouse scroll-wheel event.
+		// Only requires input on the vertical wheel-axis
+		void ProcessMouseScroll(float yoffset);
+
+	private:
+		// calculates the front vector from the Camera's (updated) Euler Angles
+		void updateCameraVectors();
+	};
+}
 #endif
